446480c605c1e96b5ed3b56b8a803fdb89beae64
subash101 <42631546+subash101@users.noreply.github.com>
2019-08-11 09:05:41 +0545

    Create validate.js

---
+++ validate.js
@@ -0,0 +1,51 @@
+function validate()
+{
+	if (document.myForm.Name.value=="")
+	 {
+	 	alert("Please provide your name");
+	 	document.myForm.Name.focus();
+	 	return false;
+	 }
+	 if (document.myForm.Email.value="") 
+	 {
+	 	alert("Please provide your Email");
+	 	document.myForm.Email.focus();
+	 		return false;
+	 else{
+	 		var validEmail = validateEmail();
+	 		document.myForm.Email.focus();
+		}
+
+	 if (document.myForm.Zip.value=""||
+	 isNaN(document.myForm.Zip.value="")||
+	 document.myForm.Zip.value.length !=5) 
+	 {
+	 	alert("Please provide a zip in the format #####");
+	 	document.myForm.Zip.focus();
+	 	return false;
+	 }
+
+	 if (document.myForm.Country.value=="-1") 
+	 {
+	 	alert("Please provide your Country");
+	 	document.myForm.Country.focus();
+	 	return false;
+
+	 }
+	 return (true);
+}
+
+function validateEmail()
+{
+	var emailID = document.myForm.Email.value;
+	atpos = emailID.indexOf("@");
+	dotpos = emailID.lastIndexOf(".");
+
+	if(atpos < 1 || (dotpos - atpos < 2 ))
+	{
+		alert("Please enter correct email ID")
+		document.myForm.Email.focus();
+		return false;
+	}
+	return (true);
+}
